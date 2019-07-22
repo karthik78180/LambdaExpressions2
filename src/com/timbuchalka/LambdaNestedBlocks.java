6e5a8e476f69a8f0b3a698ce44ddb52859726926
karthik78180 <karthik78180@gmail.com>
2019-07-21 21:28:21 -0500

    Java 8 features (Lambda Expressions Nested Blocks, functional Interfaces and Predicate)

---
+++ src/com/timbuchalka/LambdaNestedBlocks.java
@@ -0,0 +1,55 @@
+package com.timbuchalka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdaNestedBlocks {
+
+    public static void main(String[] args) {
+
+        Employee john = new Employee("John Doe", 30);
+        Employee tim = new Employee("Tim Buchalka", 21);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(tim);
+        employees.add(jack);
+        employees.add(snow);
+
+        AnotherClass anotherClass = new AnotherClass();
+        String s = anotherClass.doSomething();
+        System.out.println(s);
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
+    }
+}
+
+interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
+}
+
+class AnotherClass {
+
+    public String doSomething() {
+        int i = 0;
+
+        UpperConcat uc = (s1, s2) -> {
+            System.out.println("The lambda expression's class is " + getClass().getSimpleName());
+            String result = s1.toUpperCase() + s2.toUpperCase();
+            return result;
+        };
+
+        System.out.println("The AnotherClass class's name is " + getClass().getSimpleName());
+        return LambdaNestedBlocks.doStringStuff(uc,"String1","String2");
+    }
+}
+
+
+
+
+
+
